023c6265b9c2df6c8ea364eea30d2ca16fe7b34c
admin <iyiliang@foxmail.com>
2020-03-13 15:14:39 +0800

    学习练的代码添加到git

--- src/testOne/testTwo.java
+++ src/testOne/testTwo.java
@@ -17,7 +17,7 @@ public class testTwo {
 	}
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
+
 		int[] arr = new int[] {5,1,6,4,2,8,9};
 		int min = getMin(arr);
 		System.out.println("min:"+min);
